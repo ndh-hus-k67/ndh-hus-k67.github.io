61600c1a81192784bd61f7391526bfca17b7653d
dhuy198 <dnhuy198@gmail.com>
2026-03-09 17:14:36 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   // 1. CẬP NHẬT TÊN MIỀN CỦA BẠN
-  site: "https://hahutech.me",
+  site: "https://hahutech.com.vn",
   
   output: "static",
   

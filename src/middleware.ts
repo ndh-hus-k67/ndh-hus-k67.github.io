3062265b0afa00d5a4134b72e4314b83de59ff91
ndh-hus-k67 <nguyenduyhuy_t67@hus.edu.vn>
2026-08-15 08:32:36 +0700

    Delete src/middleware.ts

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async ({ url }, next) => {
-  if (url.pathname === "/404") {
-    return next();
-  }
-
-  return Response.redirect(new URL("/404", url), 404);
-});
